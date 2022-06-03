40dbcaf88f40b6000b79489ffe22b483c0f7ef25
Kristjan Mändmets <kristjan.mandmets@ppe.ee>
2022-06-03 14:17:31 +0300

    uus

---
+++ DirectoriesAndFiles/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoriesAndFiles
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            string rootDirectory = @"C:\Users\...\Samples";
+            Console.WriteLine("Enter directory name:");
+
+            string newDirectory = Console.ReadLine();
+            Console.WriteLine("Enter file name:");
+            string fileName = Console.ReadLine();
+
+
+            if (System.IO.Directory.Exists($"{rootDirectory}\\{newDirectory}") && System.IO.File.Exists($"{rootDirectory}\\{newDirectory}\\{fileName}"))
+            {
+                Console.WriteLine($"Directory and File exist.");
+            }
+            else
+            {
+				System.IO.Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+                File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+            }
+        }
+	}
+}
---
+++ ShoppingList/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ShoppingList
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            string fileLocation = @"C:\Users\...\Samples\Ostunimekiri";
+            string fileName = @"\\Ostunimekiri.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> myWishList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add a product to your shopping list? Y/N:");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter product:");
+                    string userWish = Console.ReadLine();
+                    myWishList.Add(userWish);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Take care!");
+                }
+
+            }
+
+            Console.Clear();
+
+            foreach (string wish in myWishList)
+            {
+                Console.WriteLine($"Your product: {wish}");
+            }
+
+            File.WriteAllLines($"{fileLocation}{fileName}", myWishList);
+        }
+	}
+}
